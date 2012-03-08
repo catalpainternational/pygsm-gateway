ed164f0f997f6f81a3659e7a406cea0140f53058
Anders Hofstee <a.hofstee@catalpainternational.org>
2012-03-08 15:14:50 +0900

    Now we can auto-detect HUAWEI modems on both Windows and MacOS X

--- bin/pygsm-gateway.py
+++ bin/pygsm-gateway.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import platform
 import logging
 
 from pygsm_gateway.http import PygsmHttpServer
 from pygsm_gateway.gsm import GsmPollingThread
+from serial.tools import list_ports
 
 
 logger = logging.getLogger()
@@ -12,19 +14,41 @@ logger.setLevel(logging.INFO)
 fabulaws_logger = logging.getLogger('pygsm_gateway')
 fabulaws_logger.setLevel(logging.DEBUG)
 
+
+### Auto detect and configure for Huawei 3G/GSM modem
+
+# get platform name
+platform_name = platform.platform()
+
+# If the platform is windows then we cycle through the COM ports
+# until we find the right interface
+if platform_name.startswith('Windows'):
+    for c in list_ports.comports():
+        if c[1].find('PC UI Interface') != -1:
+            port = c[0]
+# If the platform is MacOS X / Darwin then we just set the default name
+elif platform.platform().startswith('Darwin'):
+    port = '/dev/tty.HUAWEIMobile-Modem'
+# Linux should be somethign like this but this still needs to be fleshed out
+else:
+    port = '/dev/tty.USB1'
+
+
 if __name__ == '__main__':
     args = {
-        'url': 'http://localhost:8000/backend/pygsm-gateway/',
+        #'url': 'http://localhost:8000/backend/pygsm-gateway/',  # for use with rapidsms-threadless-router
+        'url': 'http://localhost:8000/router/receive',  # for use with rapidsms-httprouter
         'url_args': {},
         'modem_args': {
-            'port': '/dev/ttyACM0',
-            'baudrate': '115200',
-            'rtscts': '1',
-            'timeout': '10',
+            'port': port,
+            'baudrate': 115200,
+            'rtscts': 1,
+            'timeout': 10,
         }
     }
     gsm_thread = GsmPollingThread(**args)
     gsm_thread.start()
+
     server = PygsmHttpServer(('localhost', 8080), gsm_thread.send)
     print 'Starting server, use <Ctrl-C> to stop'
     try:
